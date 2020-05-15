f7bdcd6e3911d9aca940ab9ad1bfde13f5aede0d
Brian Bockelman <bbockelm@cse.unl.edu>
2020-05-15 14:52:39 -0500

    Finish fleshing out the plugins.

--- src/XrdClAuthzPlugin.cc
+++ src/XrdClAuthzPlugin.cc
@@ -166,241 +166,184 @@ private:
     XrdCl::File m_file;
 };
 
-  //----------------------------------------------------------------------------
-  //! An interface for file plug-ins
-  //----------------------------------------------------------------------------
-  class FileSystemPlugIn
-  {
-    public:
-      //------------------------------------------------------------------------
-      //! Destructor
-      //------------------------------------------------------------------------
-      virtual ~FileSystemPlugIn() {}
+class FileSystem : public XrdCl::FileSystemPlugIn
+{
+public:
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Locate
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Locate( const std::string &path,
-                                   OpenFlags::Flags   flags,
-                                   ResponseHandler   *handler,
-                                   uint16_t           timeout )
-      {
-        (void)path; (void)flags; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    FileSystem(const std::string &url) : m_fs(url) {}
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::DeepLocate
-      //------------------------------------------------------------------------
-      virtual XRootDStatus DeepLocate( const std::string &path,
-                                       OpenFlags::Flags   flags,
-                                       ResponseHandler   *handler,
-                                       uint16_t           timeout )
-      {
-        (void)path; (void)flags; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual ~FileSystem() {}
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Mv
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Mv( const std::string &source,
-                               const std::string &dest,
-                               ResponseHandler   *handler,
-                               uint16_t           timeout )
-      {
-        (void)source; (void)dest; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Locate(
+        const std::string &path,
+        OpenFlags::Flags   flags,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.Locate(path, flags, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Query
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Query( QueryCode::Code  queryCode,
-                                  const Buffer    &arg,
-                                  ResponseHandler *handler,
-                                  uint16_t         timeout )
-      {
-        (void)queryCode; (void)arg; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Mv(
+        const std::string &source,
+        const std::string &dest,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.Mv(source, dest, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Truncate
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Truncate( const std::string &path,
-                                     uint64_t           size,
-                                     ResponseHandler   *handler,
-                                     uint16_t           timeout )
-      {
-        (void)path; (void)size; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Query(
+        QueryCode::Code  queryCode,
+        const Buffer    &arg,
+        ResponseHandler *handler,
+        uint16_t         timeout)
+    override
+    {
+        return m_fs.Query(queryCode, arg, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Rm
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Rm( const std::string &path,
-                               ResponseHandler   *handler,
-                               uint16_t           timeout )
-      {
-        (void)path; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Truncate(
+        const std::string &path,
+        uint64_t           size,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.Truncate(path, size, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::MkDir
-      //------------------------------------------------------------------------
-      virtual XRootDStatus MkDir( const std::string &path,
-                                  MkDirFlags::Flags  flags,
-                                  Access::Mode       mode,
-                                  ResponseHandler   *handler,
-                                  uint16_t           timeout )
-      {
-        (void)path; (void)flags; (void)mode; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Rm(
+        const std::string &path,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.Rm(path, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::RmDir
-      //------------------------------------------------------------------------
-      virtual XRootDStatus RmDir( const std::string &path,
-                                  ResponseHandler   *handler,
-                                  uint16_t           timeout )
-      {
-        (void)path; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus MkDir(
+        const std::string &path,
+        MkDirFlags::Flags  flags,
+        Access::Mode       mode,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.MkDir(path, flags, mode, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::ChMod
-      //------------------------------------------------------------------------
-      virtual XRootDStatus ChMod( const std::string &path,
-                                  Access::Mode       mode,
-                                  ResponseHandler   *handler,
-                                  uint16_t           timeout )
-      {
-        (void)path; (void)mode; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus RmDir(
+        const std::string &path,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.RmDir(path, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Ping
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Ping( ResponseHandler *handler,
-                                 uint16_t         timeout )
-      {
-        (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus ChMod(
+        const std::string &path,
+        Access::Mode       mode,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.ChMod(path, mode, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Stat
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Stat( const std::string &path,
-                                 ResponseHandler   *handler,
-                                 uint16_t           timeout )
-      {
-        (void)path; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Ping(
+        ResponseHandler *handler,
+        uint16_t         timeout)
+    override
+    {
+        return m_fs.Ping(handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::StatVFS
-      //------------------------------------------------------------------------
-      virtual XRootDStatus StatVFS( const std::string &path,
-                                    ResponseHandler   *handler,
-                                    uint16_t           timeout )
-      {
-        (void)path; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Stat(
+        const std::string &path,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    {
+        return m_fs.Stat(path, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Protocol
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Protocol( ResponseHandler *handler,
-                                     uint16_t         timeout = 0 )
-      {
-        (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus StatVFS(
+        const std::string &path,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    {
+        return m_fs.StatVFS(path, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::DirlList
-      //------------------------------------------------------------------------
-      virtual XRootDStatus DirList( const std::string   &path,
-                                    DirListFlags::Flags  flags,
-                                    ResponseHandler     *handler,
-                                    uint16_t             timeout )
-      {
-        (void)path; (void)flags; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus Protocol(
+        ResponseHandler *handler,
+        uint16_t         timeout = 0)
+    override
+    {
+        return m_fs.Protocol(handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::SendInfo
-      //------------------------------------------------------------------------
-      virtual XRootDStatus SendInfo( const std::string &info,
-                                     ResponseHandler   *handler,
-                                     uint16_t           timeout )
-      {
-        (void)info; (void)handler; (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus DirList(
+        const std::string   &path,
+        DirListFlags::Flags  flags,
+        ResponseHandler     *handler,
+        uint16_t             timeout)
+    override
+    {
+        return m_fs.DirList(path, flags, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::Prepare
-      //------------------------------------------------------------------------
-      virtual XRootDStatus Prepare( const std::vector<std::string> &fileList,
-                                    PrepareFlags::Flags             flags,
-                                    uint8_t                         priority,
-                                    ResponseHandler                *handler,
-                                    uint16_t                        timeout )
-      {
-        (void)fileList; (void)flags; (void)priority; (void)handler;
-        (void)timeout;
-        return XRootDStatus( stError, errNotImplemented );
-      }
+    virtual XRootDStatus SendInfo(
+        const std::string &info,
+        ResponseHandler   *handler,
+        uint16_t           timeout)
+    override
+    {
+        return m_fs.SendInfo(info, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::SetProperty
-      //------------------------------------------------------------------------
-      virtual bool SetProperty( const std::string &name,
-                                const std::string &value )
-      {
-        (void)name; (void)value;
-        return false;
-      }
+    virtual XRootDStatus Prepare(
+        const std::vector<std::string> &fileList,
+        PrepareFlags::Flags             flags,
+        uint8_t                         priority,
+        ResponseHandler                *handler,
+        uint16_t                        timeout)
+    override
+    {
+        return m_fs.Prepare(fileList, flags, priority, handler, timeout);
+    }
 
-      //------------------------------------------------------------------------
-      //! @see XrdCl::FileSystem::GetProperty
-      //------------------------------------------------------------------------
-      virtual bool GetProperty( const std::string &name,
-                                std::string &value ) const
-      {
-        (void)name; (void)value;
-        return false;
-      }
-  };
+    virtual bool SetProperty(
+        const std::string &name,
+        const std::string &value)
+    override
+    {
+        return m_fs.SetProperty(name, value);
+    }
+
+    virtual bool GetProperty(
+        const std::string &name,
+        std::string &value) const
+    override
+    {
+        return m_fs.GetProperty(name, value);
+    }
+
+private:
+    XrdCl::FileSystem m_fs;
+};
 
 class Factory
 {
-    public:
-      //------------------------------------------------------------------------
-      //! Destructor
-      //------------------------------------------------------------------------
-      virtual ~Factory() {}
+public:
 
-      //------------------------------------------------------------------------
-      //! Create a file plug-in for the given URL
-      //------------------------------------------------------------------------
-      virtual FilePlugIn *CreateFile( const std::string &url ) {return new File();}
+    virtual ~Factory() {}
 
-      //------------------------------------------------------------------------
-      //! Create a file system plug-in for the given URL
-      //------------------------------------------------------------------------
-      virtual FileSystemPlugIn *CreateFileSystem( const std::string &url ) {return nullptr;}
+    virtual FilePlugIn *CreateFile( const std::string &url ) {return new File();}
+
+    virtual FileSystemPlugIn *CreateFileSystem( const std::string &url ) {return new FileSystem(url);}
 };
 
 }
